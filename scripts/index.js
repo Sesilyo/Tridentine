8aefe337138c1b4fef59abd2a77c56db9ac9356e
Sesilyo <c.sethleander@gmail.com>
2025-06-02 21:38:35 +0800

    Made fetch() work and started on the Introduction

---
+++ scripts/index.js
@@ -0,0 +1,8 @@
+fetch('external_pages/introduction.html')
+    .then(response => response.text())
+    .then(html => {
+        document.getElementById('content').innerHTML = html;
+    })
+    .catch(error => {
+        console.error('Error fetching external HTML:', error);
+    });
