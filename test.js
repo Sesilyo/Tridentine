de50a43dd5af43562099d51e27e018dbe60e1d35
Sesilyo <c.sethleander@gmail.com>
2025-06-11 07:51:20 +0800

    Made lots of changes

---
+++ test.js
@@ -0,0 +1,8 @@
+fetch('\\pages\\mass_externals\\1_catechumens\\credo.html')
+    .then(response => response.text())
+    .then(html => {
+        document.getElementById('content').innerHTML = html;
+    })
+    .catch(error => {
+        console.error('Error fetching external HTML:', error);
+    });
