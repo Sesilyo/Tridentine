43484e0e4167bda4235ed56127eb728ff836c2ed
Sesilyo <c.sethleander@gmail.com>
2025-06-16 10:56:38 +0800

    Fixed fetch().

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-fetch('pages/external_pages/introduction.html')
+fetch('pages/mass_externals/1_catechumens/1_prayers-at-the-foot-of-the-altar.html')
     .then(response => response.text())
     .then(html => {
         document.getElementById('content').innerHTML = html;
@@ -7,6 +7,7 @@ fetch('pages/external_pages/introduction.html')
         console.error('Error fetching external HTML:', error);
     });
 
+/*
 fetch('pages/mass_externals/1_catechumens/credo.html')
     .then(response => response.text())
     .then(html => {
@@ -15,3 +16,5 @@ fetch('pages/mass_externals/1_catechumens/credo.html')
     .catch(error => {
         console.error('Error fetching external HTML:', error);
     });
+
+*/
